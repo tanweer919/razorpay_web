9ea5908a81c4c2676b83e7879d8b0b0a4295b8e7
Tanweer anwar <tanweer.anwar919@gmail.com>
2025-07-03 16:36:47 +0530

    feat: new update

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:razorpay_flutter/razorpay_flutter.dart';
-
-import 'package:fluttertoast/fluttertoast.dart';
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
